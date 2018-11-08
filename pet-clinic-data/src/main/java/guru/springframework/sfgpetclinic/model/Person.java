21d6ff1122d8692ad24b14b2bf4866ffc7fa491d
Patrick McCamish <patrick.mccamish@gmx.com>
2018-11-08 12:05:10 -0500

    Implement Interfaces for Services

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,10 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Person {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String firstName;
 	private String lastName;
 
