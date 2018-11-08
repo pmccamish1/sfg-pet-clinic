629eeab14eee5e465ba1737e99ac64b926ec21d5
Patrick McCamish <patrick.mccamish@gmx.com>
2018-11-08 17:27:23 -0500

    Implement map based services

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -2,10 +2,14 @@ package guru.springframework.sfgpetclinic.services;
 
 import java.util.Set;
 
-public interface CrudService<T,ID> {
-	T findById(ID iD);
-	T save(T entity);
-	Set<T> findAll();
-	void delete(T entity);
-	void deleteById(ID id);
+public interface CrudService<T, ID> {
+	public T findById(ID iD);
+
+	public T save(T entity);
+
+	public Set<T> findAll();
+
+	public void delete(T entity);
+
+	public void deleteById(ID id);
 }
