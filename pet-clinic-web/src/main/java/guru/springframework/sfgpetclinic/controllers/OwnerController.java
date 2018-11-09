09bf5826b9ffeee97e2ec41b33e03c79aa591378
Patrick McCamish <patrick.mccamish@gmx.com>
2018-11-09 16:40:04 -0500

    Populate index views for owner and vet

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,26 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
+	private final OwnerService ownerService;
+	
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
 
-	@RequestMapping({"/owners","/owners/index","/owners/index.html"})
-	public String index() {
+
+	@RequestMapping({"/owners","/owners/","/owners/index","/owners/index.html"})
+	public String index(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
+	
 }
